13bee43fbd38dfd0d1c0c34a07e161c05ad2b558
Felix <felix@iron59.co.uk>
2019-09-03 11:49:50 +0100

    formatting

--- lib/pages/duration.dart
+++ lib/pages/duration.dart
@@ -28,106 +28,71 @@ class _DurationPageState extends State<DurationPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).primaryColor,
-      appBar: AppBar(
-        title: Text("durations"),
-        actions:
-          [
-            Switch(
-            value: value,
-            onChanged: (val) {
-              setState(() {
-                value = val;
-                DynamicTheme.of(context).setBrightness(val ? Brightness.dark: Brightness.light);
-              });
-            },
-            activeColor: DynamicTheme.of(context).data.accentColor,
-            inactiveThumbColor: DynamicTheme.of(context).data.accentColor,
-            inactiveTrackColor: DynamicTheme.of(context).data.accentColor.withOpacity(0.6),
-          ),
-        ],
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(
-            bottom: Radius.circular(8),
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).requestFocus(new FocusNode());
+      },
+      child: Scaffold(
+        backgroundColor: Theme.of(context).primaryColor,
+        appBar: AppBar(
+          title: Text("durations"),
+          actions:
+            [
+              Switch(
+              value: value,
+              onChanged: (val) {
+                setState(() {
+                  value = val;
+                  DynamicTheme.of(context).setBrightness(val ? Brightness.dark: Brightness.light);
+                });
+              },
+              activeColor: DynamicTheme.of(context).data.accentColor,
+              inactiveThumbColor: DynamicTheme.of(context).data.accentColor,
+              inactiveTrackColor: DynamicTheme.of(context).data.accentColor.withOpacity(0.6),
+            ),
+          ],
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(
+              bottom: Radius.circular(8),
+            ),
           ),
         ),
-      ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.only(top: 10),
-              width: MediaQuery.of(context).size.width,
-              height: 30,
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    width: MediaQuery.of(context).size.width / numColumns,
-                    child: Center(child: Text("D", style: Theme.of(context).textTheme.body1.copyWith(fontSize: 20))),
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width / numColumns,
-                    child: Center(child: Text("H", style: Theme.of(context).textTheme.body1.copyWith(fontSize: 20))),
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width / numColumns,
-                    child: Center(child: Text("M", style: Theme.of(context).textTheme.body1.copyWith(fontSize: 20))),
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width / numColumns,
-                    child: Center(child: Text("S", style: Theme.of(context).textTheme.body1.copyWith(fontSize: 20))),
-                  )
-                ],
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.only(top: 10),
+                width: MediaQuery.of(context).size.width,
+                height: 30,
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      width: MediaQuery.of(context).size.width / numColumns,
+                      child: Center(child: Text("D", style: Theme.of(context).textTheme.body1.copyWith(fontSize: 20))),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width / numColumns,
+                      child: Center(child: Text("H", style: Theme.of(context).textTheme.body1.copyWith(fontSize: 20))),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width / numColumns,
+                      child: Center(child: Text("M", style: Theme.of(context).textTheme.body1.copyWith(fontSize: 20))),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width / numColumns,
+                      child: Center(child: Text("S", style: Theme.of(context).textTheme.body1.copyWith(fontSize: 20))),
+                    )
+                  ],
+                ),
               ),
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: 60,
-              child : Row(
-                children: [
-                  Container(
-                    width: MediaQuery.of(context).size.width / numColumns,
-                    child: TextField(
-                      keyboardType: TextInputType.number,
-                      textAlign: TextAlign.center,
-                      style: Theme.of(context).textTheme.body1.copyWith(fontSize: 22),
-                      decoration: InputDecoration(
-                        border: UnderlineInputBorder(borderSide: BorderSide(color: Theme.of(context).accentColor)),
-                        focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Theme.of(context).accentColor)),
-                        hintText: "000",
-                      )
-                    ),
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width / numColumns,
-                    child: TextField(
-                      keyboardType: TextInputType.number,
-                      textAlign: TextAlign.center,
-                      style: Theme.of(context).textTheme.body1.copyWith(fontSize: 22),
-                      decoration: InputDecoration(
-                        border: UnderlineInputBorder(borderSide: BorderSide(color: Theme.of(context).accentColor)),
-                        focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Theme.of(context).accentColor)),
-                        hintText: "000",
-                      )
-                    ),
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width / numColumns,
-                    child: TextField(
-                      keyboardType: TextInputType.number,
-                      textAlign: TextAlign.center,
-                      style: Theme.of(context).textTheme.body1.copyWith(fontSize: 22),
-                      decoration: InputDecoration(
-                        border: UnderlineInputBorder(borderSide: BorderSide(color: Theme.of(context).accentColor)),
-                        focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Theme.of(context).accentColor)),
-                        hintText: "000",
-                      )
-                    ),
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width / numColumns,
-                    child: TextField(
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: 60,
+                child : Row(
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width / numColumns,
+                      child: TextField(
                         keyboardType: TextInputType.number,
                         textAlign: TextAlign.center,
                         style: Theme.of(context).textTheme.body1.copyWith(fontSize: 22),
@@ -136,12 +101,52 @@ class _DurationPageState extends State<DurationPage> {
                           focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Theme.of(context).accentColor)),
                           hintText: "000",
                         )
+                      ),
                     ),
-                  ),
-                ],
+                    Container(
+                      width: MediaQuery.of(context).size.width / numColumns,
+                      child: TextField(
+                        keyboardType: TextInputType.number,
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.body1.copyWith(fontSize: 22),
+                        decoration: InputDecoration(
+                          border: UnderlineInputBorder(borderSide: BorderSide(color: Theme.of(context).accentColor)),
+                          focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Theme.of(context).accentColor)),
+                          hintText: "000",
+                        )
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width / numColumns,
+                      child: TextField(
+                        keyboardType: TextInputType.number,
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.body1.copyWith(fontSize: 22),
+                        decoration: InputDecoration(
+                          border: UnderlineInputBorder(borderSide: BorderSide(color: Theme.of(context).accentColor)),
+                          focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Theme.of(context).accentColor)),
+                          hintText: "000",
+                        )
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width / numColumns,
+                      child: TextField(
+                          keyboardType: TextInputType.number,
+                          textAlign: TextAlign.center,
+                          style: Theme.of(context).textTheme.body1.copyWith(fontSize: 22),
+                          decoration: InputDecoration(
+                            border: UnderlineInputBorder(borderSide: BorderSide(color: Theme.of(context).accentColor)),
+                            focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Theme.of(context).accentColor)),
+                            hintText: "000",
+                          )
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
